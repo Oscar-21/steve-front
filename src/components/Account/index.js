5d2411b45c26f4c57eb185757c78873e9356cf50
Ubuntu <ubuntu@react>
2017-08-12 17:31:47 +0000

    added EventStore comp. cleaned Account and NavBAr

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-/* var React = require('react'); */
 
 class Account extends React.Component {
   constructor() {
@@ -24,9 +23,6 @@ class Account extends React.Component {
     this.setState({ date: e.target.value });
   }
 
-  showToken = () => {
-    console.log(this.state.token);
-  }	
 
   storeMeeting = () => {
     const data = new FormData();
@@ -49,145 +45,18 @@ class Account extends React.Component {
     });
   }
 
-  	
-
- /*componentWillMount() {
-    fetch('http:/localhost:8000/api/userName'
-    ).then( response => {
-      return response.json();
-    }).then( user => {
-      this.setState({ userName: user })    
-    });
-    console.log('userName set and component mounted');
-  } 
-
-
-  componentWillMount() {
-    fetch('http://localhost:8000/api/userName'
-    ).then(function(response) {
-      return response.json();
-    }).then(function(user) {
-      this.setState({ userName: user }) 
-    }.bind(this));
-   
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.userName !== this.state.userName) {
-      this.setState({ markup: 
-        <div>
-          <h1>{this.state.userName} Account Information</h1> 
-          <input style={{width: '40%'}} type="text" onChange={this.changeUserName} />
-          <button style={{width: '40%'}} onClick={this.bob}>Button</button>
-        </div>
-      });
-      console.log('userName updated and component rerendered');
-    } else if (prevState.userName === this.state.userName) {
-      console.log('userName not updated');
-    }
-  }
-
-  
-  changeUserName = function(e) {
-    this.setState({userName: e.target.value}); 
-  }.bind(this);
-
-  changeUserName = e => {
-    this.setState({userName: e.target.value}); 
-  } 
-
-  bob = () => {
-    this.setState({userName: 'Bob'});
-  }
-
-
-foo = () => {
-  let names = [];
-  for (const name of Object.getOwnPropertyNames(Account)) {
-    names.push(name);
-  }
-  console.log(names);
-}
-
-  CheckIfUserSet = () => {
-    const markup =
-
-      React.createElement(
-        'div',
-        null,
-
-          React.createElement(
-            'h1',
-            null,
-            this.state.userName,
-            ' Account Information'
-          ),
-
-          React.createElement(
-            'input', 
-            { 
-              style: { width: '40%' }, 
-              type: 'text', 
-              onChange: this.changeUserName 
-            }
-          ),
-
-          React.createElement(
-            'button',
-            { 
-              style: { width: '40%' }, 
-              onClick: this.bob 
-            },
-            'Button'
-          ),
-      );
-
-    if (this.state.userName !== '') {
-      console.log(' return markup');
-      return (
-        markup
-      );
-    } else {
-      console.log( 'this.state.userName = \'\'');
-    }
-  } 
-
-  CheckIfUserSet = () => {
-    const markup =
-        <div>
-          {<h1 key={0}>{this.state.userName} Account Information</h1>} 
-          {<input key={1} style={{width: '40%'}} type="text" onChange={this.changeUserName} />}
-          {<button key={2} style={{width: '40%'}} onClick={this.bob}>Button</button>}
-        </div>
-
-    if (this.state.userName !== '') {
-      console.log(' return markup');
-      return (
-        markup
-      );
-    } else {
-      console.log( 'this.state.userName = \'\'');
-    }
-  } */
   render() {
-    /*const style = {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    };*/
 
     return ( 
       <div>
-        {/*<div style={style}>
-          this.CheckIfUserSet()
-        </div>*/}
-        {/*<button onClick={this.foo}  >button</button>*/}
-	{/*<a href="http://localhost:8000/api/test">button</a>*/}
-
+	
+	<h1>Create Event</h1>	
 	<input type="text" name="name" onChange={this.handleName} />
 	<input type="text" name="category" onChange={this.handleCategory} />
 	<input type="text" name="date" onChange={this.handleDate} />
 	<button onClick={this.storeMeeting}>Button</button>
-        <button onClick={this.showToken}>Token</button>
+
+	<hr />
 
       </div>
     );
---
+++ src/components/EventStore/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class EventStore extends Component {
+  render() {
+    return (
+		
+    ); 
+  }
+}
